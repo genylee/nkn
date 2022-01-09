288f0263ca4fed3cfc2d8cbc8cebe33774c462e5
genylee <94005206+genylee@users.noreply.github.com>
2022-01-10 02:15:32 +0800

    Add files via upload

--- cwallet.sh
+++ cwallet.sh
@@ -3,11 +3,14 @@
 # 钱包数量
 num=30
 # 钱包密码
-password="KentLovelincent0709"
+password="Gentlovelincent0709"
 
-
+rm -r /root/wallet
+mkdir /root/wallet
 a=1
 while [[ a -le num ]]; do
-	/home/nknx/nkn-commercial/services/nkn-node/nknc wallet -c -n wallet${a}.json -p ${password}
+	mkdir /root/wallet/wallet${a}
+	cd /root/wallet/wallet${a}
+	/home/nknx/nkn-commercial/services/nkn-node/nknc wallet -c -n wallet.json -p ${password}
 	let "a++"
 done
